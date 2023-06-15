154affe43adfa4c87c0bf37785ca541af50bcbc9
Rerurate514 <rerurateyuto@gmail.com>
2023-06-15 11:14:53 +0900

    interface-test

--- sayHello.ts
+++ sayHello.ts
@@ -1,5 +1,44 @@
-const arr = ['adwafa','thfh','y54','bdx 5yer',':@p:p@pp'];
+interface Point {
+    x: number;
+    y?: number;
+}
+
+type Label = {
+    [key: string] : string
+}
+
+const labels: Label = {
+    toptitle: "4242",
+    topFeature: "32424",
+    topSubTitle: "23rfessrfer"
+}
+
+class MyPonit implements Point {
+    x: number = 0;
+}
+
+let originP : Point = {x:43,y:53}
+
+const arr : string[] = ['adwafa','thfh','y54','bdx 5yer',':@p:p@pp'];
 
 arr.forEach((name) => {
     console.log(name)
+    console.log(originP)
 })
+
+interface ColorFul {
+    color: string;
+}
+
+interface Circle {
+    radius: number;
+}
+
+interface ColorFulCircle extends ColorFul, Circle {}
+
+const cc: ColorFulCircle = {
+    color: 'red',
+    radius: 10
+}
+
+console.log(cc);
--- test/sayHello.js
+++ test/sayHello.js
@@ -1,10 +1,23 @@
 "use strict";
+const labels = {
+    toptitle: "4242",
+    topFeature: "32424",
+    topSubTitle: "23rfessrfer"
+};
+class MyPonit {
+    constructor() {
+        this.x = 0;
+    }
+}
+let originP = { x: 43, y: 53 };
 const arr = ['adwafa', 'thfh', 'y54', 'bdx 5yer', ':@p:p@pp'];
 arr.forEach((name) => {
     console.log(name);
+    console.log(originP);
 });
-window.confirm = () => {
-    console.log('confirm');
-    return true;
+const cc = {
+    color: 'red',
+    radius: 10
 };
+console.log(cc);
 //# sourceMappingURL=sayHello.js.map
